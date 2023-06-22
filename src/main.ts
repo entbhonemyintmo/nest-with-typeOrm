f584f48abd237422eab6c33222ae0227eab9f3ec
entbhonemyintmo <entbhonemyintmo@gmail.com>
2023-06-22 22:40:35 +0630

    feat(main): swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs With TypeORM')
+    .setDescription(
+      'this is my nestjs study project using TypeORM instead of Prisma',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(8001);
 }
 bootstrap();
